28629e124068a031ec369270fdc8fcd3dbaa09fc
alex <alextavares1943@gmail.com>
2025-10-07 21:43:45 -0300

    Projeto Semestral

---
+++ projeto-semestral/src/main/java/projeto/semestral/projeto_semestral/ProjetoSemestralApplication.java
@@ -0,0 +1,13 @@
+package projeto.semestral.projeto_semestral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSemestralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSemestralApplication.class, args);
+	}
+
+}
---
+++ projeto-semestral/src/test/java/projeto/semestral/projeto_semestral/ProjetoSemestralApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.semestral.projeto_semestral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSemestralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
